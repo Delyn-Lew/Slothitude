71bf3f991e16886131a89e462d0b2a8fe28f01ee
delynlpy@gmail.com <delynlpy@gmail.com>
2024-05-30 15:34:16 +0800

    setup log in and log out page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,41 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AuthPage({ setUser }) {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const togglePW = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const navigateToSignUp = () => {
+    setShowSignUp(true);
+  };
+
+  const navigateToDash = () => {
+    navigate("/dashboard");
+  };
   return (
     <>
-      <LoginForm setUser={setUser} />
-      <SignUpForm setUser={setUser} />
+      {!showSignUp ? (
+        <LoginForm
+          setUser={setUser}
+          onSignUp={navigateToSignUp}
+          showPassword={showPassword}
+          togglePW={togglePW}
+        />
+      ) : (
+        <SignUpForm
+          setUser={setUser}
+          navigateToDash={navigateToDash}
+          showPassword={showPassword}
+          togglePW={togglePW}
+        />
+      )}
     </>
   );
 }
