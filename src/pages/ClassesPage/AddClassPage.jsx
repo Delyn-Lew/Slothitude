7ab6fcd14a27624c1570c7a99d8b626454551092
delynlpy@gmail.com <delynlpy@gmail.com>
2024-06-06 16:59:54 +0800

    feat: onclick add class button will go to another page to add class information

---
+++ src/pages/ClassesPage/AddClassPage.jsx
@@ -0,0 +1,12 @@
+import AddClassForm from "./AddClassForm";
+
+export default function AddClassPage() {
+  return (
+    <div className="flex justify-center">
+      <section className="text-xl bg-white bg-opacity-80 w-[100rem] p-5 rounded-lg flex flex-col justify-center items-center drop-shadow-xl">
+        <h2 className="text-center">Add Class</h2>
+        <AddClassForm />
+      </section>
+    </div>
+  );
+}
