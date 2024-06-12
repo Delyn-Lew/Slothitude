d3b689e58c1b757ef8c9ea034ca5300e3917f912
delynlpy@gmail.com <delynlpy@gmail.com>
2024-06-12 14:35:28 +0800

    feat: choosing font

--- src/pages/BookingSummaryPage/BookingSummaryPage.jsx
+++ src/pages/BookingSummaryPage/BookingSummaryPage.jsx
@@ -14,7 +14,21 @@ export default function BookingSummaryPage({ user }) {
         log("Fetching user bookings");
         const bookings = await fetchUserBookings(userId);
         log("Fetched bookings:", bookings);
-        setBookedClasses(bookings);
+        // Filter classes into upcoming and passed classes
+        const upcomingClasses = [];
+        const passedClasses = [];
+        const today = new Date();
+        bookings.forEach((booking) => {
+          const classDate = new Date(booking.classId?.date);
+          if (classDate < today) {
+            passedClasses.push(booking);
+          } else {
+            upcomingClasses.push(booking);
+          }
+        });
+        // Concatenate upcoming and passed classes
+        const sortedBookings = upcomingClasses.concat(passedClasses);
+        setBookedClasses(sortedBookings);
       } catch (error) {
         log("Error fetching user bookings", error);
       }
@@ -24,7 +38,6 @@ export default function BookingSummaryPage({ user }) {
 
   return (
     <div className="flex justify-center mt-16">
-      {/* Add margin-top to push content down */}
       <section className="text-xl bg-white bg-opacity-80 w-[100rem] p-5 rounded-lg flex flex-col justify-center items-center drop-shadow-xl">
         <h2 className="text-center">My Bookings</h2>
         {bookedClasses.length > 0 ? (
@@ -59,7 +72,11 @@ export default function BookingSummaryPage({ user }) {
                 {bookedClasses.map((booking) => (
                   <tr
                     key={booking._id}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    className={`${
+                      new Date(booking.classId?.date) < new Date()
+                        ? "text-gray-600 bg-gray-200"
+                        : ""
+                    } bg-white border-b dark:bg-gray-800 dark:border-gray-700`}
                   >
                     <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                       {booking.classId?.name || "N/A"}
