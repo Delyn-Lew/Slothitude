45c7e616d4dbf500aabf6cbe711430fbca21ef96
delynlpy@gmail.com <delynlpy@gmail.com>
2024-06-12 20:41:33 +0800

    feat: move image from navbar to beside header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,17 @@ import ClassesCalendar from "./ClassesCalendar";
 export default function HomePage() {
   return (
     <div className="pt-16">
-      <h1 className="text-3xl font-bold text-center my-4">
-        Welcome to Slothitude
-      </h1>
+      <div className="flex items-center justify-center my-2">
+        <h1 className="text-3xl font-bold text-center">
+          Welcome to Slothitude
+        </h1>
+        <img
+          src="Slothitude.png"
+          className="ml-4"
+          alt="Slothitude Logo"
+          style={{ height: "90px", width: "auto" }}
+        />
+      </div>
       <ClassesCalendar />
       <Outlet />
     </div>
